dc8b74fa2a969dcf942cef42e0ed285495708384
Teo Temonen <teo.temonen@proton.me>
2026-06-01 19:23:36 +0300

    chore: remove unused re-export barrels (lib/api, lib/types/external)

--- src/lib/api/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./discord";
-export * from "./external";
-export * from "./keymap";
-export * from "./twitch";
-export * from "./utils";
--- src/lib/types/external/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./emote";
-export * from "./robotty";
-export * from "./seventv";
